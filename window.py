e0059be1a712122f088ce7e06b0842e6a8d3b3e4
Ghanadri, Michel <michel.ghanadri@intel.com>
2025-02-12 21:23:22 +0200

    created general button interface for OS API

--- window.py
+++ window.py
@@ -24,28 +24,99 @@ class Window():
         background_image = pygame.image.load(rf"media\background.jpg")
         background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))  # Scale to fit the window
 
-        button_rect = pygame.Rect(20, 20, 100, 100)  # x, y, width, height
-        button_color = RED
+        # Button properties
+        # button_rect = pygame.Rect(20, 20, 100, 100)  # x, y, width, height
+        # button_color = RED
+
+        prime_button = Button(20, 20, 100, 100, RED, 'Prime95', self.prime_button_action)
+        b2 = Button(140, 20, 100, 100, RED, 'n', self.prime_button_action)
+        buttons_list = [prime_button, b2]
 
         self._running = True
         while self._running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    self._running = False
                     self._windows_events.append('exit')
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    # check if the button is clicked
-                    if button_rect.collidepoint(event.pos):
-                        self._windows_events.append('start prime95')
-                        print("Button clicked!")
-                        button_color = GREEN
+                    # Check if the button is clicked
+                    for button in buttons_list:
+                        if button.layout.collidepoint(event.pos):
+                            button.click()
 
+                    # if b2.layout.collidepoint(event.pos):
+                    #     b2.click()
+                        
+                    # if button_rect.collidepoint(event.pos):
+                    #     self._windows_events.append('start prime95')
+                    #     print("Button clicked!")
+                    #     button_color = GREEN  # Change button color to green
+
+            # Draw the background image
             screen.blit(background_image, (0, 0))
-            pygame.draw.rect(screen, button_color, button_rect)
-            font = pygame.font.Font(None, 24)
-            text = font.render("Prime95", True, BLACK)
-            text_rect = text.get_rect(center=button_rect.center)
-            screen.blit(text, text_rect)
+
+            # Draw the button
+            for button in buttons_list:
+                button.draw(screen)
+
+            # pygame.draw.rect(screen, button_color, button_rect)
+            # b2.draw(screen)
+
+            # Draw button text
+            # font = pygame.font.Font(None, 24)
+            # text = font.render("Prime95", True, BLACK)
+            # text_rect = text.get_rect(center=button_rect.center)
+            # screen.blit(text, text_rect)
+
             pygame.display.flip()
 
         pygame.quit()
+
+    def create_button(self, x, y, width, height, color, test):
+        button_rect = pygame.Rect(20, 20, 100, 100)  # x, y, width, height
+        button_color = RED
+
+    def prime_button_action(self):
+        self._windows_events.append('start prime95')
+        print("Prime95 Button clicked!")
+        # button_color = GREEN  # Change button color to green
+
+
+
+class Button():
+    def __init__(self, x, y, width, height, color=(255, 255, 255), text='', on_click_action=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.text = text
+        self._on_click_action = on_click_action
+
+        self.layout = None
+        self.text_box = None
+        self.update_layout()
+        self.update_text()
+        return
+
+    def update_layout(self):
+        self.layout = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self, screen):
+        if self.layout:
+            pygame.draw.rect(screen, self.color, self.layout)
+
+        if self.text_box:
+           screen.blit(self._text_render, self.text_box)
+
+    def update_text(self):
+        if self.text != '':
+            font = pygame.font.Font(None, 24)
+            self._text_render = font.render(self.text, True, (0, 0, 0))
+            self.text_box = self._text_render.get_rect(center=self.layout.center)
+
+    def click(self):
+        if self._on_click_action is not None:
+            self._on_click_action()
+            self.color = (0, 255, 0)
