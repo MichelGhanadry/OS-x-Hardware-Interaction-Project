fbb4b91928ec65367f1355ae9db204786601a2a7
Ghanadri, Michel <michel.ghanadri@intel.com>
2025-02-12 21:31:03 +0200

    created os config file and changed GUI implementations

--- window.py
+++ window.py
@@ -1,6 +1,7 @@
 import pygame
 import threading
 import sys
+from windows_config import *
 
 class Window():
     def __init__(self, windows_events):
@@ -8,19 +9,23 @@ class Window():
         self._running = False
         self._thread = threading.Thread(target=self.run_window)
         self._thread.start()
+        self._running_prime = False
 
     def run_window(self):
+        # Initialize Pygame
         pygame.init()
 
+        # Screen dimensions
         WIDTH, HEIGHT = 800, 600
         screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("System")
         pygame.mouse.set_visible = False
-        # colors
+        # Colors
         BLACK = (0, 0, 0)
         RED = (255, 0, 0)
         GREEN = (0, 255, 0)
 
+        # Load background image
         background_image = pygame.image.load(rf"media\background.jpg")
         background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))  # Scale to fit the window
 
@@ -28,10 +33,11 @@ class Window():
         # button_rect = pygame.Rect(20, 20, 100, 100)  # x, y, width, height
         # button_color = RED
 
-        prime_button = Button(20, 20, 100, 100, RED, 'Prime95', self.prime_button_action)
-        b2 = Button(140, 20, 100, 100, RED, 'n', self.prime_button_action)
-        buttons_list = [prime_button, b2]
+        prime_button = Button(20, 20, 100, 100, text='Prime95', action=self.prime_button_action)
+        # b2 = Button(140, 20, 100, 100, RED, 'n', self.prime_button_action)
+        buttons_list = [prime_button]
 
+        # Main loop
         self._running = True
         while self._running:
             for event in pygame.event.get():
@@ -69,36 +75,44 @@ class Window():
             # text_rect = text.get_rect(center=button_rect.center)
             # screen.blit(text, text_rect)
 
+            # Update the display
             pygame.display.flip()
 
+        # Quit Pygame
         pygame.quit()
+        # sys.exit()
 
     def create_button(self, x, y, width, height, color, test):
         button_rect = pygame.Rect(20, 20, 100, 100)  # x, y, width, height
         button_color = RED
 
-    def prime_button_action(self):
-        self._windows_events.append('start prime95')
+    def prime_button_action(self, button):
+        self._running_prime = not self._running_prime
+        state = 'start' if self._running_prime else 'stop'
+        button.update_color('green' if self._running_prime else 'red') 
+        self._windows_events.append(f'{state} prime95')
         print("Prime95 Button clicked!")
-        # button_color = GREEN  # Change button color to green
 
 
 
 class Button():
-    def __init__(self, x, y, width, height, color=(255, 255, 255), text='', on_click_action=None):
+    def __init__(self, x, y, width, height, color='red', text='', action=None):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.color = color
+        self.update_color(color)
         self.text = text
-        self._on_click_action = on_click_action
+        self._on_click_action = action
 
         self.layout = None
         self.text_box = None
         self.update_layout()
         self.update_text()
         return
+
+    def update_color(self, color):
+        self.color = colors[color]
 
     def update_layout(self):
         self.layout = pygame.Rect(self.x, self.y, self.width, self.height)
@@ -118,5 +132,4 @@ class Button():
 
     def click(self):
         if self._on_click_action is not None:
-            self._on_click_action()
-            self.color = (0, 255, 0)
+            self._on_click_action(self)
