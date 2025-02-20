a36766964b9e4dea43a8d0bae3aa24a90db5133b
Ghanadri, Michel <michel.ghanadri@intel.com>
2025-02-20 20:41:42 +0200

    adding digits to syscode

--- syscode.py
+++ syscode.py
@@ -11,12 +11,20 @@ class SysCode():
         print(BORD_HIGHT)
         self._bord = pygame.Rect(BORD_X, BORD_Y, BORD_WIGHT, BORD_HIGHT)
         self._digits_list = [Digit(i) for i in range(num_Of_digits)]
-        # pygame.Rect(self.x, self.y, self.width, self.height)
+        self.display_code()
 
     def draw(self, screen):
         pygame.draw.rect(screen, colors['white'], self._bord)
         for digit in self._digits_list:
            digit.draw(screen)
+    
+    def set_code(self, code):
+        self.code = code
+        self.display_code()
+
+    def display_code(self):
+        for i in range(self.num_Of_digits):
+            self._digits_list[i].set_char(self.code[i])
 
     
 
@@ -32,7 +40,15 @@ class Digit():
                       pygame.Rect(BORD_X+11+PIN_SHORT+D, BORD_Y+16+2*PIN_SHORT+2*PIN_LONG, PIN_LONG, PIN_SHORT),
                       pygame.Rect(BORD_X+11+PIN_SHORT+PIN_LONG+D, BORD_Y+16+2*PIN_SHORT+PIN_LONG, PIN_SHORT, PIN_LONG),
                     ]
+        self._pins_colors = ['red' for _ in range(len(self._pins))]
+        return
 
     def draw(self, screen):
-        for pin in self._pins:
-           pygame.draw.rect(screen, colors['red'], pin) 
+        for i, pin in enumerate(self._pins):
+           c = self._pins_colors[i]
+           pygame.draw.rect(screen, colors[c], pin)
+
+    def set_char(self, char):
+        if char in sysencode:
+            for index, val in enumerate(sysencode[char]):
+                self._pins_colors[index] = 'red' if val == 1 else 'gray'
