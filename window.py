08e4be4aaa53cf88ae8086ea7a4566ed2f9ace90
Ghanadri, Michel <michel.ghanadri@intel.com>
2025-02-28 23:52:20 +0200

    creating random numbers window

--- window.py
+++ window.py
@@ -4,6 +4,7 @@ import sys
 from windows_config import *
 from button import Button
 from syscode import SysCode
+from app_window import AppWindow
 
 class Window():
     def __init__(self, windows_events):
@@ -32,23 +33,35 @@ class Window():
         background_image = pygame.image.load(rf"media\background.jpg")
         background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))  # Scale to fit the window
 
+        # Button properties
+        # button_rect = pygame.Rect(20, 20, 100, 100)  # x, y, width, height
+        # button_color = RED
+
         prime_button = Button(20, 20, 100, 100, text='Prime95', action=self.prime_button_action)
         # b2 = Button(140, 20, 100, 100, RED, 'n', self.prime_button_action)
         buttons_list = [prime_button]
+        self.apps_dict = {}
 
         # Main loop
         self._running = True
         while self._running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self._running = False
                     self._windows_events.append('exit')
+                    self._exit(to_join=False)
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     # Check if the button is clicked
                     for button in buttons_list:
                         if button.layout.collidepoint(event.pos):
                             button.click()
+
+                    for app_name in self.apps_dict:
+                        app = self.apps_dict[app_name]
+                        if app:
+                            button = app.get_button()
+                            if button.layout.collidepoint(event.pos):
+                                button.click()
 
             # Draw the background image
             screen.blit(background_image, (0, 0))
@@ -57,9 +70,13 @@ class Window():
             for button in buttons_list:
                 button.draw(screen)
 
+            for app_name in self.apps_dict:
+                app = self.apps_dict[app_name]
+                if app:
+                    app.draw(screen)
+
             self.syscode.draw(screen)
 
-            # Update the display
             pygame.display.flip()
 
         # Quit Pygame
@@ -68,13 +85,36 @@ class Window():
 
     def create_button(self, x, y, width, height, color, test):
         button_rect = pygame.Rect(20, 20, 100, 100)  # x, y, width, height
-        button_color = (255, 0, 0)
-        return
+        button_color = RED
 
     def prime_button_action(self, button):
         self._running_prime = not self._running_prime
         state = 'start' if self._running_prime else 'stop'
-        button.update_color('green' if self._running_prime else 'red') 
+        button.update_color('green' if self._running_prime else 'red')
+        if self._running_prime:
+            self.apps_dict['Prime95'] = AppWindow(150,150,300,550, action=self.close, action_args=button)
+        else:
+            self.apps_dict['Prime95'].close()
+            self.apps_dict['Prime95'] = None
         self._windows_events.append(f'{state} prime95')
         print("Prime95 Button clicked!")
-        return
+
+    def close(self, button):
+        print('closing')
+        self._running_prime = not self._running_prime
+        state = 'start' if self._running_prime else 'stop'
+        button.update_color('green' if self._running_prime else 'red')
+        self.apps_dict['Prime95'].close()
+        self.apps_dict['Prime95'] = None
+        self._windows_events.append(f'{state} prime95')
+
+    def _exit(self, to_join=True):
+        for app_name in self.apps_dict:
+            app = self.apps_dict[app_name]
+            if app is not None:
+                app.close()
+
+        self._running = False
+        if to_join:
+            self._thread.join()
+        
