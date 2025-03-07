a30cad7982aa39f97440ad31bbceffd692e0f74e
Ghanadri, Michel <michel.ghanadri@intel.com>
2025-03-07 23:34:12 +0200

    adding timeline to pcode

--- pcode.py
+++ pcode.py
@@ -1,6 +1,7 @@
 from pcode_config import *
 from time import time, sleep
 import threading
+from timeline import show_timeline
 
 class Pcode():
     def __init__(self, system):
@@ -10,6 +11,7 @@ class Pcode():
         self._cpu_mode = "idle"
         self._cpu_mode_counter = 0
         self._cpu_mode_start_time = time()
+        self.states_timeline_list = [(self._cpu_mode, self._cpu_mode_start_time)]
 
         self._red_flag = False
         self._cpu_mode_thread = threading.Thread(target=self._cpu_mode_check)
@@ -26,19 +28,23 @@ class Pcode():
             current_cpu_mode = self._get_cpu_mode()
             if current_cpu_mode == self._cpu_mode:
                 self._cpu_mode_counter += 1
-                if time() - self._cpu_mode_start_time > CPU_IDLE_TIME:
-                    self._system.window._set_sleep_mode(mode=True)
-                    self._cpu_mode = "sleep"
-                    print("go to sleep")
-            else:
-                self._cpu_mode = current_cpu_mode
-                self._cpu_mode_counter = 0
-                self._cpu_mode_start_time = time()
-            sleep(0.1)
 
+                if self._cpu_mode == "idle":
+                    if time() - self._cpu_mode_start_time > CPU_IDLE_TIME:
+                        self._system.window._set_sleep_mode(mode=True)
+                        self._switch_cpu_mode(new_mode="sleep")
+                        print("go to sleep")
+            else:
+                self._switch_cpu_mode(new_mode=current_cpu_mode)
+                
+            sleep(0.1)
+            print(self._cpu_mode)
         return
 
     def _get_cpu_mode(self):
+        if self._cpu_mode == "sleep":
+            return self._cpu_mode
+        
         for core in self._cores:
             if core.get_frequency() > CPU_IDLE_FREQ:
                 return "stress"
@@ -46,4 +52,28 @@ class Pcode():
 
     def wake_system(self):
         self._system.window._set_sleep_mode(mode=False)
+        self._switch_cpu_mode(new_mode="idle")
+
+    def _switch_cpu_mode(self, new_mode):
+        self._cpu_mode = new_mode
+        self._cpu_mode_counter = 0
+        self._cpu_mode_start_time = time()
+        self.states_timeline_list.append((self._cpu_mode, self._cpu_mode_start_time))
+
+
+    
+    def create_timeline(self):
+        timelines = []
+        prev_state = self.states_timeline_list[0]
+        for state in self.states_timeline_list[1:] + [(self._cpu_mode , time())]:
+            print(state)
+            state_duration = state[1] - prev_state[1]
+            prev_state_end_time = self.states_timeline_list[-1][1]
+            timelines.append((prev_state_end_time, prev_state_end_time+state_duration, state[0]))
+            prev_state = state
+
+        print(self.states_timeline_list)
+        print(timelines)
+        show_timeline(data=timelines, states=["sleep", "idle", "stress"])
+
 
